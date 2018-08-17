e6f3b4018881bfe3027e245195d245bcfc53d551
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2018-08-17 15:57:01 +0100

    Spawn child

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,28 @@ fn main() {
     let stage = env::args().nth(1).expect("too few arguments");
     match stage.as_ref() {
         "run" => run(),
+        "child" => child(),
         _ => panic!("bad command"),
     }
 }
 
 fn run() {
+    let args = env::args().skip(2).collect::<Vec<String>>();
+
+    println!("Running [{}]", args.join(" "));
+
+    let mut child = Command::new("/proc/self/exe")
+                            .args(&["child"])
+                            .args(&args)
+                            .unshare(&[Namespace::Uts])
+                            .spawn()
+                            .expect(&format!("/proc/self/exe failed to start"));
+
+    let _ecode = child.wait()
+                      .expect("failed to wait on child");
+}
+
+fn child() {
     let cmd = env::args().nth(2).expect("missing command");
     let args = env::args().skip(3).collect::<Vec<String>>();
 
